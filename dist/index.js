07935a3166b737d42a0edfa07e8e7e215ada63a6
Conor Butler <cwdbutler@gmail.com>
2022-01-07 16:36:46 +0000

    Express setup, working endpoint

--- dist/index.js
+++ dist/index.js
@@ -8,21 +8,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const express_1 = __importDefault(require("express"));
 const prisma = new client_1.PrismaClient();
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const post = yield prisma.post.create({
-            data: {
-                caption: "another post",
-            },
-        });
-        const allPosts = yield prisma.post.findMany();
-        console.dir(allPosts);
-    });
-}
-main().catch((e) => {
-    throw e;
+const app = (0, express_1.default)();
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`);
 });
+// async function main() {
+//   // const post = await prisma.post.create({
+//   //   data: {
+//   //     caption: "another post",
+//   //   },
+//   // });
+//   const allPosts = await prisma.post.findMany();
+//   console.dir(allPosts);
+// }
+// main().catch((e) => {
+//   throw e;
+// });
+app.get("/", (_, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const allPosts = yield prisma.post.findMany();
+    res.send(allPosts);
+}));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
 
 const prisma = new PrismaClient();
+const app = express();
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
 
-async function main() {
-  const post = await prisma.post.create({
-    data: {
-      caption: "another post",
-    },
-  });
+// async function main() {
+//   // const post = await prisma.post.create({
+//   //   data: {
+//   //     caption: "another post",
+//   //   },
+//   // });
 
+//   const allPosts = await prisma.post.findMany();
+//   console.dir(allPosts);
+// }
+
+// main().catch((e) => {
+//   throw e;
+// });
+
+app.get("/", async (_, res) => {
   const allPosts = await prisma.post.findMany();
-  console.dir(allPosts);
-}
-
-main().catch((e) => {
-  throw e;
+  res.send(allPosts);
 });
