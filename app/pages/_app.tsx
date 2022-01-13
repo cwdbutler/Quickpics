fa4deac69692cdff2d764e01f3935aac77064e6e
Conor Butler <cwdbutler@gmail.com>
2022-01-13 16:40:06 +0000

    Working cookie

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,6 +4,9 @@ import { createClient, Provider } from "urql";
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
