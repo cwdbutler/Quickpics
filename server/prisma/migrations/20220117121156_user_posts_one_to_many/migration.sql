edd545015335f5c3dc0992066b592f3e26f03aca
Conor Butler <cwdbutler@gmail.com>
2022-01-17 12:12:13 +0000

    User to posts relationship in db

---
+++ server/prisma/migrations/20220117121156_user_posts_one_to_many/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "authorId" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
