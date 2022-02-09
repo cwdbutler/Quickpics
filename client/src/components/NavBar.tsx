b4eb6003d89f1cefc15e71fa8b058c433f9b197d
Conor Butler <cwdbutler@gmail.com>
2022-02-09 19:30:41 +0000

    Navbar modal bugfix

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -18,68 +18,70 @@ export default function NavBar() {
   const router = useRouter();
 
   return !mounted ? null : (
-    <nav className="bg-white sticky top-0 z-50 border-b-[1px] border-gray-300">
-      <div className="max-w-[990px] mx-auto px-6">
-        <div className="relative flex items-center justify-between h-14">
-          <Link href="/">
-            <a className="p-2 flex-shrink-0">
-              <h1 className="font-logo text-3xl">Quickpics</h1>
-            </a>
-          </Link>
-          <div className="flex-1 flex items-stretch justify-end mr-2">
-            {fetching || isServer() ? null : data?.currentUser ? (
-              // user is logged in
-              <div className="flex items-center justify-center space-x-4">
-                <Link href="/">
-                  <a>
-                    {router.pathname === "/" ? (
-                      <HomeIconFilled className="h-8" />
-                    ) : (
-                      <HomeIcon className="h-8 stroke-1.5" />
-                    )}
-                  </a>
-                </Link>
-                <Link href="/create">
-                  <a>
-                    {router.pathname === "/create" ? (
-                      <PlusIconFilled className="h-8" />
-                    ) : (
-                      <PlusIcon className="h-8" />
-                    )}
-                  </a>
-                </Link>
-                <NavBarDropDown user={data.currentUser} />
-              </div>
-            ) : (
-              <div className="flex space-x-4">
-                <button
-                  onClick={() => {
-                    router.push({
-                      pathname: "/login",
-                      query: { from: router.asPath },
-                    });
-                  }}
-                  className="px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Log in
-                </button>
+    <div className="pt-[56px]">
+      <nav className="bg-white w-full top-0 fixed z-10 border-b-[1px] border-gray-300">
+        <div className="max-w-[990px] mx-auto px-6">
+          <div className="relative flex items-center justify-between h-14">
+            <Link href="/">
+              <a className="p-2 flex-shrink-0">
+                <h1 className="font-logo text-3xl">Quickpics</h1>
+              </a>
+            </Link>
+            <div className="flex-1 flex items-stretch justify-end mr-2">
+              {fetching || isServer() ? null : data?.currentUser ? (
+                // user is logged in
+                <div className="flex items-center justify-center space-x-4">
+                  <Link href="/">
+                    <a>
+                      {router.pathname === "/" ? (
+                        <HomeIconFilled className="h-8" />
+                      ) : (
+                        <HomeIcon className="h-8 stroke-1.5" />
+                      )}
+                    </a>
+                  </Link>
+                  <Link href="/create">
+                    <a>
+                      {router.pathname === "/create" ? (
+                        <PlusIconFilled className="h-8" />
+                      ) : (
+                        <PlusIcon className="h-8" />
+                      )}
+                    </a>
+                  </Link>
+                  <NavBarDropDown user={data.currentUser} />
+                </div>
+              ) : (
+                <div className="flex space-x-4">
+                  <button
+                    onClick={() => {
+                      router.push({
+                        pathname: "/login",
+                        query: { from: router.asPath },
+                      });
+                    }}
+                    className="px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Log in
+                  </button>
 
-                <button
-                  onClick={() => {
-                    router.push({
-                      pathname: "/register",
-                      query: { from: router.asPath },
-                    });
-                  }}
-                  className="px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Sign up
-                </button>
-              </div>
-            )}
+                  <button
+                    onClick={() => {
+                      router.push({
+                        pathname: "/register",
+                        query: { from: router.asPath },
+                      });
+                    }}
+                    className="px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
