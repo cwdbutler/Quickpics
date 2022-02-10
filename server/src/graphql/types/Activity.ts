c9e6c6f2768510c4f19bfd1e459c55041e8afc7f
Conor Butler <cwdbutler@gmail.com>
2022-02-10 10:58:46 +0000

    Remove unused Activity resolver

--- server/src/graphql/types/Activity.ts
+++ server/src/graphql/types/Activity.ts
@@ -1,14 +1,16 @@
-import { createUnionType } from "type-graphql";
-import { Post } from "./Post";
+// import { createUnionType } from "type-graphql";
+// import { Post } from "./Post";
 
-export const ActivityUnion = createUnionType({
-  name: "Activity", // the name of the GraphQL union
-  types: () => [Post] as const, // will also contain comments soon
-  // detecting returned object type
-  resolveType: (value) => {
-    if ("caption" in value) {
-      return Post; // return the relevant `@ObjectType()`
-    }
-    return undefined;
-  },
-});
+// unused feature; was planning to have a feed of all activity
+
+// export const ActivityUnion = createUnionType({
+//   name: "Activity", // the name of the GraphQL union
+//   types: () => [Post] as const, // will also contain comments soon
+//   // detecting returned object type
+//   resolveType: (value) => {
+//     if ("caption" in value) {
+//       return Post; // return the relevant `@ObjectType()`
+//     }
+//     return undefined;
+//   },
+// });
